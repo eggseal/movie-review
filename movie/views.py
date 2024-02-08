6cb8a78064f1103eb0733d16b45500af656dee24
BuildTools <nicosm2004@gmail.com>
2024-02-07 19:44:07 -0500

    Initial Commit

---
+++ movie/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def home(request):
+    args = {
+        'name': 'Greg Lim'
+    }
+    return render(request, 'home.html', args)
+
+def about(request):
+    return HttpResponse("<h1>Welcome to About Us</h1>")
