ad836efae4bc001a32b094e9a8e9a747cf93229f
BuildTools <nicosm2004@gmail.com>
2024-02-07 20:09:03 -0500

    Movie filtering

--- movie/views.py
+++ movie/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Movie
+
 # Create your views here.
 def home(request):
+    search = request.GET.get('search')
+    movies = Movie.objects.filter(title__contains=search)
     args = {
-        'name': 'Nicolás Sanchez'
+        'search': search,
+        'movies': movies
     }
     return render(request, 'home.html', args)
 
